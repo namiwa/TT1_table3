b2f58859977b552b55556af143fd4e5fc188c8a9
xiao-yangg <xiaoyangg2020@outlook.com>
2024-01-20 15:55:42 +0800

    feat: integrate linkage

--- Frontend/client/src/pages/Dashboard.jsx
+++ Frontend/client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import DashboardTable from '../components/DashboardTable';
 import './Dashboard.css';
 
+import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
 import DestinationModal from "../components/DestinationModal";
 
@@ -9,13 +10,15 @@ const Dashboard = () => {
   const [openModal, setOpenModal] = useState(false);
   const handleOpen = () => setOpenModal(true);
 
+  const navigate = useNavigate()
+
   return (
     <section className='dashboard-page container'>
       <Button onClick={handleOpen}>Open modal</Button>
       {openModal && (
         <DestinationModal openModal={openModal} setOpenModal={setOpenModal} />
       )}
-        <button className='add-button'>ADD ITENARY</button>
+        <button className='add-button' onClick={() => {navigate("/itinerary");}}>ADD ITENARY</button>
         <DashboardTable />
     </section>
   )
