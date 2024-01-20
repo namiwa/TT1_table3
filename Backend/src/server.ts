fdfab7538213215ca5e8262944bd74afc49fa5f8
Benjamin <benjaminchoo97@gmail.com>
2024-01-20 14:03:44 +0800

    Update server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,45 +1,33 @@
-import {
-  connectIternaryToCountry,
-  createCountry,
-  createDestination,
-  createUserItinerary,
-  getUser,
-  newUser,
-} from "./repository/testRepository";
+import exp from "constants";
 
 const countryRoutes = require("./routes/countryRoutes");
 const itineraryRoutes = require("./routes/itineraryRoutes");
+const destinationRoutes = require("./routes/destinationRoutes");
+const PORT = process.env.PORT;
 
 require("dotenv").config();
 import cors from "cors";
 import express from "express";
-
-const detinationRoutes = require("./routes/destinationRoutes");
-
-const PORT = process.env.PORT;
-const app = express();
-require('dotenv').config();
-import cors from "cors";
 import corsOptions from "./config/cors/corsOption";
 import authRoutes from "./routes/authRoutes";
 import passport from "passport";
 
 require('./config/passport strategy/passportJWT')(passport)
+const app = express();
 app.use(passport.initialize()); // Create the passport strategy object
-
-app.use("/country", countryRoutes);
-app.use("/itinerary", itineraryRoutes);
-app.use("/destination", detinationRoutes);
 
 app.use(express.json());
 app.use(cors(corsOptions))
 
 app.use("/auth", authRoutes )
+app.use("/country", countryRoutes);
+app.use("/itinerary", itineraryRoutes);
+app.use("/destination", destinationRoutes);
 
 
 
 
-const val = getUser()
+
 
 app.listen(PORT, () => console.log(`listening to port ${PORT} `)); // This port should be in env
 
