4fb24e6f4e9905ac892df25dd8edef8d5bf534dd
xiao-yangg <xiaoyangg2020@outlook.com>
2024-01-20 12:32:12 +0800

    fix: merge Charmaine branch

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -3,15 +3,18 @@ import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import Itinerary from './pages/Itinerary';
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/" element={<Navigate to="/itinerary" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/home" element={<Dashboard />} />
+          <Route path="/itinerary" element={<Itinerary />} />
+          <Route path="*" element={<Itinerary />} />
         </Routes>
       </BrowserRouter>
   );
