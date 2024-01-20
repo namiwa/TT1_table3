b87a076074215b0597e44484c7f432a07efb6006
Chong Wei Meng <87300821+wmchong@users.noreply.github.com>
2024-01-20 13:55:37 +0800

    destination

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -11,8 +11,12 @@ require("dotenv").config();
 import cors from "cors";
 import express from "express";
 
+const detinationRoutes = require('./routes/destinationRoutes');
+
 const PORT = process.env.PORT;
 const app = express();
+
+app.use('/destination', detinationRoutes);
 
 app.use(express.json());
 const val = getUser();
